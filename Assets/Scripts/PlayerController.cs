c916745f8b06e89d1010fef09845a7d70ea2168d
RdotSilva <RyandotSilva@gmail.com>
2021-10-21 15:21:26 -0400

    Add logic to increase torque on keypress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+     [SerializeField] float torqueAmount = 1f;
+    Rigidbody2D rb2d;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
     }
 }
