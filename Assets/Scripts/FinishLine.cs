516d901c13bd700c459b2183e44d3a1d4266e5cc
RdotSilva <RyandotSilva@gmail.com>
2021-10-22 15:20:47 -0400

    Add TODO for crossing finish line

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void OnTriggerEnter2D(Collider2D other) 
+   {
+       Debug.Log("You have finished!");
+       // TODO: Add logic for when a player crosses finish line
+   }
 }
