1a0b27930def1d307865688ca35f3e7bf8ca78f6
RdotSilva <RyandotSilva@gmail.com>
2021-10-23 07:40:26 -0400

    Add OnTriggerEnter2D for head bump logic

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+      if (other.tag == "Ground")
+      {
+          Debug.Log("You bumped your head!");
+          // TODO: Add logic to reset game when player bumps head
+      }
+  }
 }
