b44fc8ec82083728fda57643accecf838f09a0e2
RdotSilva <RyandotSilva@gmail.com>
2021-10-25 09:02:06 -0400

    Add logic for player bumping head

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
+  LevelGenerator levelGenerator;
   private void OnTriggerEnter2D(Collider2D other)
   {
       if (other.tag == "Ground")
       {
           Debug.Log("You bumped your head!");
           // TODO: Add logic to reset game when player bumps head
+          // For now just restart the level
+          levelGenerator.OnFinishLevel();
       }
   }
 }
