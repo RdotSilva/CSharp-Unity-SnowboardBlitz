9a5e64a82687d866f6c0bbae61581c6b4d0d5850
RdotSilva <RyandotSilva@gmail.com>
2021-10-26 14:19:39 -0400

    Refactor reloading using ReloadScene method

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,12 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+  [SerializeField] float sceneReloadDelay = 0.5f; 
   private void OnTriggerEnter2D(Collider2D other)
   {
       if (other.tag == "Ground")
       {
           // Reload the scene when user bumps head
-        SceneManager.LoadScene(0);
+        Invoke("ReloadScene", sceneReloadDelay);
       }
   }
+
+   /// <summary>
+   ///   Reload the current scene
+   /// </summary>
+   private void ReloadScene()
+   {
+      SceneManager.LoadScene(0);
+   }
 }
