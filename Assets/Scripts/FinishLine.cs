d9ed1bcf9be76c69f9534622f77a7d283eca5904
RdotSilva <RyandotSilva@gmail.com>
2021-10-26 14:17:06 -0400

    Refactor using sceneReloadDelay

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+   [SerializeField] float sceneReloadDelay = 1f; 
    private void OnTriggerEnter2D(Collider2D other) 
    {
       if (other.tag == "Player")
       {
            // Reload the scene when user crosses the finish line
-           Invoke("ReloadScene", 2f);
+           Invoke("ReloadScene", sceneReloadDelay);
            // TODO: Intead of restarting the scene we should load into the next level
       }
    }
