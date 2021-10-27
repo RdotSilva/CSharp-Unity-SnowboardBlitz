2691a471bd98214e206e7f9a2b86f2052b2a4c3c
RdotSilva <RyandotSilva@gmail.com>
2021-10-27 15:36:14 -0400

    Add logic to trigger particle effect on finish

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,13 +6,17 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
    [SerializeField] float sceneReloadDelay = 1f; 
+   [SerializeField] ParticleSystem finishEffect;
    private void OnTriggerEnter2D(Collider2D other) 
    {
       if (other.tag == "Player")
       {
-           // Reload the scene when user crosses the finish line
-           Invoke("ReloadScene", sceneReloadDelay);
-           // TODO: Intead of restarting the scene we should load into the next level
+
+         // Play particle effect when user crosses finish line
+         finishEffect.Play();
+         // Reload the scene when user crosses the finish line
+         Invoke("ReloadScene", sceneReloadDelay);
+         // TODO: Intead of restarting the scene we should load into the next level
       }
    }
 
