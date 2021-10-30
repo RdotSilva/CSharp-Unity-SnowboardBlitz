9ae698d978c6c5f38b5920b6ccd870e251389300
RdotSilva <RyandotSilva@gmail.com>
2021-10-30 08:33:28 -0400

    Add logic to play dust effect

--- Assets/Scripts/DustTrail.cs
+++ Assets/Scripts/DustTrail.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class DustTrail : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] ParticleSystem dustParticles;
+
+    // Trigger particle effect when player is touching ground
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.tag == "Ground")
+        {
+            dustParticles.Play();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
