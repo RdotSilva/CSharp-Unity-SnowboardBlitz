f880569508ca602d0e01cc3e0a962c076c968d13
RdotSilva <RyandotSilva@gmail.com>
2021-10-30 08:35:31 -0400

    Add logc to stop dust effect

--- Assets/Scripts/DustTrail.cs
+++ Assets/Scripts/DustTrail.cs
@@ -6,12 +6,21 @@ public class DustTrail : MonoBehaviour
 {
     [SerializeField] ParticleSystem dustParticles;
 
-    // Trigger particle effect when player is touching ground
+    // Trigger particle effect when player is touching
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ground")
         {
             dustParticles.Play();
+        }
+    }
+
+    // Stop the particle effect when player is off the ground
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            dustParticles.Stop();
         }
     }
 
