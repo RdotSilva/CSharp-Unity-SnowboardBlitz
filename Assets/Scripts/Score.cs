75b609f529ceec9d29c584ac510e41b745b5aa4e
RdotSilva <RyandotSilva@gmail.com>
2021-11-04 15:41:33 -0400

    Refactor score text

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,18 +5,18 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    public Text scoreText;
+    public Text score;
     private int scoreNum;
 
     void Start()
     {
         scoreNum = 0;
-        scoreText.text = "Score: " + scoreNum;
+        score.text = "Score: " + scoreNum;
     }
 
     public void IncrementScore()
     {
         scoreNum++;
-        scoreText.text = "Score: " + scoreNum;
+        score.text = "Score: " + scoreNum;
     }
 }
